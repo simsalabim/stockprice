17984969f79221dc84a140e8f3cd2241c4a2d2d3
Alexander Kaupanin <kaupanin@gmail.com>
2017-03-05 22:26:46 -0600

    Initial commit

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ `uname -s` == "Darwin" ];then
-  curl -L -s https://github.com/simsalabim/stockprice/raw/master/bin/stockprice -o /usr/local/bin/stockprice
-elif [ `uname -s` == "Linux"];then
-  curl -L -s https://github.com/simsalabim/stockprice/raw/master/bin/stockprice-lin -o /usr/local/bin/stockprice
-fi
-chmod a+x /usr/local/bin/stockprice
-echo "\`stockprice\` is now installed and available on your system."
